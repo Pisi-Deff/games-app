80097aa3de3bf8f2dd93890ae15a9ab270abcee8
Eerik Mägi <eerik46@gmail.com>
2019-08-30 01:17:01 +0300

    frontend: expect GameDetailed to come with GameReviews

---
+++ games-frontend/src/app/games/common/game-review.ts
@@ -0,0 +1,8 @@
+export interface GameReview {
+	id: number;
+	dudeUuid: string;
+	dudeDisplayName: string;
+	score: number;
+	review: string;
+	reviewDate: string;
+}
--- games-frontend/src/app/games/game-details/game-detailed.ts
+++ games-frontend/src/app/games/game-details/game-detailed.ts
@@ -1,10 +1,14 @@
 import {GameBasic} from '../common/game-basic';
 import {GameTag} from '../common/game-tag';
 import {Slice} from '../../shared/slice';
+import {Page} from '../../shared/page';
 import {GameTagging} from '../common/game-tagging';
+import {GameReview} from '../common/game-review';
 
 export interface GameDetailed extends GameBasic {
 	description: string;
 	tags: Slice<GameTag>;
 	dudeTaggings: GameTagging[];
+	reviews: Page<GameReview>;
+	dudeReview: GameReview;
 }
