d4808918e2cf94fa7924ae6e1462ed70613c3fc5
Eerik Mägi <eerik46@gmail.com>
2019-09-05 22:20:23 +0300

    use ENV for db host in api dockerfile

--- games-api/wait-for-db.sh
+++ games-api/wait-for-db.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-host="$1"
-shift
 cmd="$@"
 
-until PGPASSWORD=$DB_PASSWORD psql -h "$host" -d "$DB" -U "$DB_USER" -c '\q'; do
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -d "$DB" -U "$DB_USER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
